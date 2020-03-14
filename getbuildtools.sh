0e221b1e7960dfc234431edfe207995a55e8b794
evil maniac <evilmaniac@gmail.com>
2020-03-14 23:28:31 +0000

    adding getbuildtools.sh

---
+++ getbuildtools.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Where dependencies will be downloaded
+DOWNLOAD_LOCATION="dependencies"
+
+# Sourcemod download version & URL
+SOURCEMOD_VERSION="1.9"
+SOURCEMOD_BASE_DOWNLOAD_URL="https://sm.alliedmods.net/smdrop/"
+SOURCEMOD_LATEST="sourcemod-latest-linux"
+
+SOURCEMOD_DOWNLOAD_DIR=$SOURCEMOD_BASE_DOWNLOAD_URL$SOURCEMOD_VERSION
+SOURCEMOD_LATEST_RELEASE_NAME=$(wget -O- -q $SOURCEMOD_DOWNLOAD_DIR/$SOURCEMOD_LATEST)
+SOURCEMOD_DOWNLOAD_URL=$SOURCEMOD_DOWNLOAD_DIR/$SOURCEMOD_LATEST_RELEASE_NAME
+
+if [ ! -f $DOWNLOAD_LOCATION/$SOURCEMOD_LATEST_RELEASE_NAME ]; then
+    wget -P $DOWNLOAD_LOCATION $SOURCEMOD_DOWNLOAD_URL
+fi
+
+tar -zxvf $DOWNLOAD_LOCATION/$SOURCEMOD_LATEST_RELEASE_NAME \
+    addons/sourcemod/scripting/include \
+    addons/sourcemod/scripting/spcomp \
+    addons/sourcemod/scripting/compile.sh
